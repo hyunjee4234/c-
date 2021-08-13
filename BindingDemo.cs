ac025aa9e52e4056eb2ec3a843373f7450b1e84a
hyunjee4234 <56611411+hyunjee4234@users.noreply.github.com>
2021-08-12 20:07:41 -0700

    Add files via upload

---
+++ BindingDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class CParent
+{
+    public int N;
+    public   virtual  void Foo()
+    {
+        Console.WriteLine("In Parent");
+    }
+}
+class CChild : CParent
+{
+    public new int N;
+    public new virtual  void Foo()
+    {
+        Console.WriteLine("In Child");
+    }
+}
+class CGrandChild : CChild
+{
+    public override void Foo()
+    {
+        Console.WriteLine("In GrandChild");
+    }
+}
+class BindingDemoProgram
+{
+    static void Main(string[] args)
+    {
+        CParent p = new CChild();
+        p.Foo();
+       // p.N = 10;
+        ((CChild)p).N = 10;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Parent
+{
+    public int PubA;
+    private int PriA;
+    protected int ProA;
+    public Parent()
+    {
+    }
+    public Parent(int priA, int proA, int pubA)
+    {
+        PriA = priA;
+        ProA = proA;
+        PubA = pubA;
+    }
+}
+
+class Child : Parent
+{
+    public int PubB;
+    public Child()
+    {
+     }
+    public Child(int priA, int proA, int pubA, int pubB)
+        : base(priA, proA, pubA)
+    {
+        //PriA = priA;
+        //ProA = proA;
+        //PubA = pubA;
+        PubB = pubB;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Parent p1,p2;
+        Child c; 
+        p1 = new Parent();
+        p2 = null;
+
+        Parent p = p2 ?? p1;
+        if (p2 == null)
+            p = p1;
+        else
+            p = p2;
+
+        c = p as Child;
+
+        if (p is Child)  //If p is ref to object of Child or GrandChild 
+            c = (Child)p;
+        else
+            c = null;
+
+        Console.WriteLine(c == null);
+
+    }
+}
+
